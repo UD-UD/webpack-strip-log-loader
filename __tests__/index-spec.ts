5511fa3b42dc6e377edd89b9270242abee1a9e31
Abhas <abhasbhattacharya2@gmail.com>
2018-04-27 15:29:32 +0530

    Remove default modules and tests

--- __tests__/index-spec.ts
+++
@@ -1,9 +0,0 @@
-const mockWind = jest.fn<Window>();
-const anyGlobal = global as any;
-anyGlobal.window = new mockWind();
-
-import * as index from '../src/index';
-
-test('Should have Greeter available', () => {
-  expect(index.Greeter).toBeTruthy();
-});
