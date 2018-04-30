f70ee5437211e414ab8c413e0e1dab174b7bec4b
Abhas <abhasbhattacharya2@gmail.com>
2018-04-30 12:03:32 +0530

    Add tests - most kind of imports single + mixed.

--- __tests__/plugin-spec.ts
+++ __tests__/plugin-spec.ts
@@ -5,37 +5,226 @@ import compiler from '../src/compiler';
 const pathPreFolder = path.resolve(__dirname, '../test_files/pre');
 const pathPostFolder = path.resolve(__dirname, '../test_files/post');
 
-test('Removes namespace import statement', async () => {
-  const fileName = "import-namespace.js1";
+test('Removes default import statement', async () => {
+  const fileName = "import-default.js1";
   const pathPreFile = path.join(pathPreFolder, fileName);
   const pathPostFile = path.join(pathPostFolder, fileName);
 
-  
+
   const stats = await compiler(pathPreFile, {});
   const statsJSON = stats.toJson();
-  
+
   const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
     moduleStat.name.toLowerCase().endsWith(fileName)
   )[0].source;
-  
+
   const postFileContent = fs.readFileSync(pathPostFile).toString();
   expect(transformedPreFileContent).toBe(postFileContent);
 });
 
 
-test('Removes namespace import statement with spaced comment', async () => {
-  const fileName = "import-namespace-comment-spaced.js1";
+test('Removes default import statement with spaced comment', async () => {
+  const fileName = "import-default-comment-spaced.js1";
   const pathPreFile = path.join(pathPreFolder, fileName);
   const pathPostFile = path.join(pathPostFolder, fileName);
 
-  
+
   const stats = await compiler(pathPreFile, {});
   const statsJSON = stats.toJson();
-  
+
   const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
     moduleStat.name.toLowerCase().endsWith(fileName)
   )[0].source;
-  
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes namespace import statement', async () => {
+  const fileName = "import-namespace.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes named import statement without alias', async () => {
+  const fileName = "import-named-simple.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes named import statement with alias', async () => {
+  const fileName = "import-named-with-alias.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes imports with global strip modules', async () => {
+  const fileName = "import-global-strip.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {
+    modules: [
+      'logger-global'
+    ]
+  });
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes simple require statements', async () => {
+  const fileName = "import-require.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Doesn\'t remove invalid require statements', async () => {
+  const fileName = "import-require-invalid.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes mixed imports simple (all stripped)', async () => {
+  const fileName = "import-mixed-simple.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes mixed imports complex (some stripped + global strip modules)', async () => {
+  const fileName = "import-mixed-complex.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {
+    modules: [
+      'logger-global-2',
+      'logger-global-1'
+    ]
+  });
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes mixed imports with negative test (some stripped)', async () => {
+  const fileName = "import-mixed-with-negative.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
+  const postFileContent = fs.readFileSync(pathPostFile).toString();
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Removes explicitly marked symbols', async () => {
+  const fileName = "remove-explicit-marked-symbol.js1";
+  const pathPreFile = path.join(pathPreFolder, fileName);
+  const pathPostFile = path.join(pathPostFolder, fileName);
+
+
+  const stats = await compiler(pathPreFile, {});
+  const statsJSON = stats.toJson();
+
+  const transformedPreFileContent = (statsJSON.modules as any[]).filter(moduleStat =>
+    moduleStat.name.toLowerCase().endsWith(fileName)
+  )[0].source;
+
   const postFileContent = fs.readFileSync(pathPostFile).toString();
   expect(transformedPreFileContent).toBe(postFileContent);
 });
