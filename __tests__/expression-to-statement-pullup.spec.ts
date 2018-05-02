65110f4a67d39765be4eacbbf521f945d3510057
Abhas <abhasbhattacharya2@gmail.com>
2018-05-02 16:11:45 +0530

    Tests - add expression to statement, name hiding tests

---
+++ __tests__/expression-to-statement-pullup.spec.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import { testPrePostFile } from './utils.ignore';
+
+// Removes statement when inner expression is restricted
+
+test('Removes pulled up statement when inner expression is restricted', async () => {
+  const fileName = 'expression-to-statement-removal.js1';
+  const { transformedPreFileContent, postFileContent } = await testPrePostFile(fileName);
+
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
---
+++ __tests__/name-hiding.spec.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { testPrePostFile } from './utils.ignore';
+
+test('Hiding a restricted variable with a new variable of the same name shouldn\'t restrict new variable', async () => {
+  const fileName = 'name-hiding.js1';
+  const { transformedPreFileContent, postFileContent } = await testPrePostFile(fileName);
+
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
