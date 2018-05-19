d65ca276783cf09c218289354009643b9a15a745
Abhas <abhasbhattacharya2@gmail.com>
2018-05-19 21:29:45 +0530

    Tests - Increase timeout for unchanged

--- __tests__/no-log-unchanged.spec.ts
+++ __tests__/no-log-unchanged.spec.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import { testPrePostFile } from './utils.ignore';
 
-jest.setTimeout(15000);
+jest.setTimeout(25000);
 
 test('Files containing no restricted symbol/expression should remain unchanged', async () => {
   const fileName = 'no-log-unchanged.js1';
