05a7ffca3eaf4d7363ba6c0dd352327937ff57ac
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2019-07-13 13:41:57 +0530

    Add basic test cases for module name matching
    Only contains test cases for relative file import - exact + glob matching.
    
    Glob matching test case is supposed to fail now.

---
+++ __tests__/module-name-match.spec.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+import { testPrePostFile } from './utils.ignore';
+
+jest.setTimeout(15000);
+
+// Module name pattern matching tests
+
+test('Matches modules with relative path - exact string match', async () => {
+  const fileName = 'module-name-relative.js1';
+  const { transformedPreFileContent, postFileContent } = await testPrePostFile(
+    fileName,
+    {
+      modules: ['./local-logger'],
+    }
+  );
+
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
+
+
+test('Matches modules with relative path - glob pattern', async () => {
+  const fileName = 'module-name-relative.js1';
+  const { transformedPreFileContent, postFileContent } = await testPrePostFile(
+    fileName,
+    {
+      modules: ['./local-*'],
+    }
+  );
+
+  expect(transformedPreFileContent).toBe(postFileContent);
+});
